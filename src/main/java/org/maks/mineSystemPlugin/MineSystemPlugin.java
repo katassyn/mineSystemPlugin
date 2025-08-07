c4e8635faa369e4def88bd0d89791ac7f1d6f71c
katassyn <mastu2115@gmail.com>
2025-08-07 12:02:25 +0200

    1st

---
+++ src/main/java/org/maks/mineSystemPlugin/MineSystemPlugin.java
@@ -0,0 +1,17 @@
+package org.maks.mineSystemPlugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MineSystemPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}

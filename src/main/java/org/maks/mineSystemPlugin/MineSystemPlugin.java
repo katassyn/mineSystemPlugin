d992f0252934b1566f85dfafa813802079977a1c
Maksymilian Stuglik <mastu2115@gmail.com>
2025-08-08 08:44:33 +0200

    Add async SQLite DAO for player, pickaxe, quest and sphere data

--- src/main/java/org/maks/mineSystemPlugin/MineSystemPlugin.java
+++ src/main/java/org/maks/mineSystemPlugin/MineSystemPlugin.java
@@ -1,17 +1,49 @@
 package org.maks.mineSystemPlugin;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import org.maks.mineSystemPlugin.database.DatabaseManager;
+import org.maks.mineSystemPlugin.database.dao.PickaxesDao;
+import org.maks.mineSystemPlugin.database.dao.PlayersDao;
+import org.maks.mineSystemPlugin.database.dao.QuestsDao;
+import org.maks.mineSystemPlugin.database.dao.SpheresDao;
 
 public final class MineSystemPlugin extends JavaPlugin {
+    private DatabaseManager database;
+    private PlayersDao playersDao;
+    private PickaxesDao pickaxesDao;
+    private QuestsDao questsDao;
+    private SpheresDao spheresDao;
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        database = new DatabaseManager(this);
+        playersDao = new PlayersDao(database);
+        pickaxesDao = new PickaxesDao(database);
+        questsDao = new QuestsDao(database);
+        spheresDao = new SpheresDao(database);
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        if (database != null) {
+            database.close();
+        }
+    }
+
+    public PlayersDao getPlayersDao() {
+        return playersDao;
+    }
+
+    public PickaxesDao getPickaxesDao() {
+        return pickaxesDao;
+    }
+
+    public QuestsDao getQuestsDao() {
+        return questsDao;
+    }
+
+    public SpheresDao getSpheresDao() {
+        return spheresDao;
     }
 }
+

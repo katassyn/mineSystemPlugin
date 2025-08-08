786d95ec58d7b83b52b4538792f934f67fcb4cf0
Maksymilian Stuglik <mastu2115@gmail.com>
2025-08-08 08:47:51 +0200

    Require premium ticket for sphere type 2

--- src/main/java/org/maks/mineSystemPlugin/MineSystemPlugin.java
+++ src/main/java/org/maks/mineSystemPlugin/MineSystemPlugin.java
@@ -2,11 +2,14 @@ package org.maks.mineSystemPlugin;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import org.maks.mineSystemPlugin.command.SphereCommand;
+
 public final class MineSystemPlugin extends JavaPlugin {
 
     @Override
     public void onEnable() {
         // Plugin startup logic
+        getCommand("sphere").setExecutor(new SphereCommand());
 
     }
 

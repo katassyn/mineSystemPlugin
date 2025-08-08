b36e2dec28fc579a23708b5de6146a7fe92ca808
Maksymilian Stuglik <mastu2115@gmail.com>
2025-08-08 08:47:38 +0200

    Implement stamina manager with timed reset

--- src/main/java/org/maks/mineSystemPlugin/MineSystemPlugin.java
+++ src/main/java/org/maks/mineSystemPlugin/MineSystemPlugin.java
@@ -2,11 +2,17 @@ package org.maks.mineSystemPlugin;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+/**
+ * Main plugin class which sets up the {@link StaminaManager}.
+ */
 public final class MineSystemPlugin extends JavaPlugin {
+
+    private StaminaManager staminaManager;
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        // Initialize stamina with a default max of 100. This value may grow with quests.
+        staminaManager = new StaminaManager(100);
 
     }
 

b31da2e22feaf7ed4f4ca28ac10f2df3a2de308e
Maksymilian Stuglik <mastu2115@gmail.com>
2025-08-08 08:47:46 +0200

    Add custom tool system with durability and enchantments

--- src/main/java/org/maks/mineSystemPlugin/MineSystemPlugin.java
+++ src/main/java/org/maks/mineSystemPlugin/MineSystemPlugin.java
@@ -1,13 +1,14 @@
 package org.maks.mineSystemPlugin;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import org.maks.mineSystemPlugin.tool.ToolListener;
 
 public final class MineSystemPlugin extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        // Register listeners
+        getServer().getPluginManager().registerEvents(new ToolListener(this), this);
     }
 
     @Override

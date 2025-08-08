fb70c20b54d26970baffd7377b5d0114fa420ad7
Maksymilian Stuglik <mastu2115@gmail.com>
2025-08-08 08:47:44 +0200

    Add special block mining mechanics

--- src/main/java/org/maks/mineSystemPlugin/MineSystemPlugin.java
+++ src/main/java/org/maks/mineSystemPlugin/MineSystemPlugin.java
@@ -3,11 +3,12 @@ package org.maks.mineSystemPlugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class MineSystemPlugin extends JavaPlugin {
+    private SpecialBlockListener listener;
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        listener = new SpecialBlockListener();
+        getServer().getPluginManager().registerEvents(listener, this);
     }
 
     @Override
